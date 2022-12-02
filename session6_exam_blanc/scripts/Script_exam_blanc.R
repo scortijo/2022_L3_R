247af21f330109257364eabeb130409111be8446
scortijo <sandra.cortijo@hotmail.fr>
2022-12-02 09:50:08 +0100

    exam blanc

---
+++ session6_exam_blanc/scripts/Script_exam_blanc.R
@@ -0,0 +1,73 @@
+# EXAMEN BLANC: introduction a bioinformatique pour la biologie vegetale
+
+### 9 decembre 2021 - 8h : 9h30, duree de 1h30min
+
+
+### Quelques remarques et consignes generales :
+
+# Le lien clickable vers l'enonce complet est disponible sur moodle
+# N'oubliez pas de renommer ce script avec votre numero d'etudiant
+# Deposez ce script (et la copie d'ecran de la question 5)  
+# sur l'espace de depot Moodle quand vous avez fini.
+
+
+# ------------ Question 0 ------------------
+
+# Chargez les librairies dont vous aurez besoin
+
+
+# Definissez le working directory de R pour etre dans le dossier de l'examen.
+
+
+# Chargez le fichier DOG1_Expression_clean.txt (qui est dans le dossier /data), 
+# qui contient les donnees d'expression, dans R et sauvez le dans un objet.
+
+# ------------ Question 1 ------------------
+
+# Y a-t-il des valeurs manquantes dans les donnees? Comment le voyez vous?
+
+
+# ------------ Question 2 ------------------
+
+# Combien y-a-t-il d'ecotypes differents? Donnez la liste des ecotypes
+
+
+# ------------ Question 3 ------------------
+
+# Faites une figure représentant les niveaux d'expression des deux gènes
+# (DOG1 et NCED9) suivant le stade de développement des siliques, 
+# l'origine géographique, et les conditions de maturation des graines, 
+# et ce uniquement pour des plantes ayant poussé en jours courts à température élevée. 
+
+
+# Formulez une interpretation complete de ce que nous apprend  cette figure quant a 
+# l'influence de la geographie, et du stade de developpement sur l'expression de 
+# nos genes d'interets.
+
+
+# ------------ Question 4 ------------------
+
+# Pour verifier si cet effet est observe pour tous les genotypes, mesurez la moyenne de 
+# l'expression normalisee de DOG1 aux deux stades de developpement de la silique 
+# pour chaque genotypes, et ce uniquement pour des plantes ayant pousse en jours 
+# courts a temperature elevee.
+
+
+# Observez-vous une difference d'expression en fonction du stade de developpement de 
+# la silique pour tous les ecotypes?
+
+
+# Y a-t-il des ecotypes pour lesquels la difference est faible? Si oui, lesquels? 
+
+
+# ------------ Question 5 ------------------
+
+# En utilisant l'application [ePlant](https://bar.utoronto.ca/eplant/) de BAR de 
+# l'Universite de Toronto trouvez les informations suivantes sur le gene DOG1 :
+
+
+# Quel est le numero unique de DOG1 (du format AT1G12345)?
+
+# Dans quel tissu de la plante est ce que le gene DOG1 est exprime? 
+# Faites un screenshot de la figure de ePlant qui vous a permis de repondre 
+# et deposez le en meme temps que votre script R sur l'espace de depot Moodle.
